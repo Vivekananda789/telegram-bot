5b74aff810dc1eb0c91341237868caad654df6b0
Crazy haxers <111678641+Vivekananda789@users.noreply.github.com>
2023-06-19 13:37:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Replace 'YOUR_BOT_TOKEN' with your Telegram bot token
+const bot = new TelegramBot('5920224766:AAGaPbH7KBHuRkH4WyegKnNUeL3Bo70tjE4', { webHook: { port: port } });
+
+// Handle /start command
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const response = 'Welcome to the Math Bot! Send me any mathematical expression, and I will calculate it for you.';
+  bot.sendMessage(chatId, response);
+});
+
+// Handle incoming messages
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  // Check if the message is a valid mathematical expression
+  if (isValidExpression(messageText)) {
+    try {
+      const result = eval(messageText);
+      bot.sendMessage(chatId, `Result: ${result}`);
+    } catch (error) {
+      bot.sendMessage(chatId, 'Oops, something went wrong. Please enter a valid mathematical expression.');
+    }
+  } else {
+    bot.sendMessage(chatId, 'Invalid mathematical expression. Please try again.');
+  }
+});
+
+// Function to validate mathematical expressions
+function isValidExpression(expression) {
+  // Regular expression to check for valid mathematical expressions
+  const regex = /^[-+*/.\d\s()]+$/;
+  return regex.test(expression);
+}
+
+// Set up the webhook route
+app.post(`/telegram-bot`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
